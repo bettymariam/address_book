409340a9774bc6d29c3116cc8c9d739a4eb19dce
Betty Chempananical <bettymariam@gmail.com>
2017-04-14 13:21:56 -0700

    changed seed files

--- db/seeds/1_addresses.js
+++ db/seeds/1_addresses.js
@@ -2,14 +2,13 @@ exports.seed = function(knex, Promise) {
   return knex('addresses').del()
     .then(function () {
       return knex('addresses').insert([
-        {line_1: '5', line_2: 'Longbourne Estate', city : 'Herfordshire', zip : '87888'},
-        {line_1: '1', line_2: 'Pemberly', city : 'Derbishire', zip : '76767'},
-        {line_1: '23', line_2: 'Netherfield Park', city : 'Hertfordshire', zip : '78888'}
+        {id: 1, line_1: '5', line_2: 'Longbourne Estate', city : 'Herfordshire', zip : '87888'},
+        {id: 2,line_1: '1', line_2: 'Pemberly', city : 'Derbishire', zip : '76767'},
+        {id: 3,line_1: '23', line_2: 'Netherfield Park', city : 'Hertfordshire', zip : '78888'}
       ]);
+    }).then(() => {
+       return knex.raw(
+         "SELECT setval('addresses_id_seq', (SELECT MAX(id) FROM addresses));"
+       )
     })
-    .then(() => {
-      return knex.raw(
-        "SELECT setval('addresses_id_seq', (SELECT MAX(id) FROM addresses));"
-      );
-    });
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -4,8 +4,10 @@ var knex = require("../db")
 
 router.get('/', function(req, res, next) {
   knex.from('contacts').innerJoin('addresses', 'contacts.address_id', 'addresses.id')
+  .select('*', 'contacts.id as contact_id','addresses.id as addressID')
   .then( contacts => {
-    res.render('index', {contacts});
+    console.log(contacts);
+    res.render('index', {contacts:contacts});
   })
   .catch( error => {
     console.log(error);
@@ -14,12 +16,12 @@ router.get('/', function(req, res, next) {
 
 router.get('/new', function(req, res, next) {
   knex.from('addresses')
-  .then(result => {
-    result.forEach((el)=>{
+  .then(contact => {
+    contact.forEach((el)=>{
       el.addressJson = JSON.stringify(el);
       el.addressString = el.line_1 + ' ' + el.line_2 + ' ' + el.city + '  ' + el.zip;
     })
-    res.render('addressBook/newContact', {title: 'New Contact', result:result});
+    res.render('addressBook/newContact', {title: 'New Contact', contact:contact});
   })
   .catch( error => {
     console.log(error);
@@ -29,9 +31,11 @@ router.get('/new', function(req, res, next) {
 
 router.get('/:id', function(req, res, next) {
   knex.from('contacts').innerJoin('addresses', 'contacts.address_id', 'addresses.id')
+  .select('*', 'contacts.id as contact_id','addresses.id as addressID')
   .where('contacts.id', req.params.id)
+  .first()
   .then( contact => {
-    res.render(`addressBook/showContact`, contact[0]);
+    res.render(`addressBook/showContact`, contact);
   })
   .catch( error => {
     console.log(error);
@@ -40,9 +44,12 @@ router.get('/:id', function(req, res, next) {
 
 router.get('/edit/:id', function(req, res, next) {
   knex.from('contacts').innerJoin('addresses', 'contacts.address_id', 'addresses.id')
+  .select('*', 'contacts.id as contact_id','addresses.id as addressID')
   .where('contacts.id', req.params.id)
+  .first()
   .then( contact => {
-    res.render(`addressBook/edit`, {title: 'Edit Contact', contact : contact[0]});
+    console.log(contact);
+    res.render(`addressBook/edit`, {title: 'Edit Contact', contact});
   })
   .catch( error => {
     console.log(error);
@@ -86,31 +93,44 @@ router.post('/', (req,res,next) => {
     zip: req.body.zip
   }
   knex('addresses')
-  .insert(address)
-  .returning('id')
-  .then(id  => {
+  .where('line_1', address.line_1)
+  .andWhere('line_2', address.line_2)
+  .andWhere('city', address.city)
+  .andWhere('zip', address.zip)
+  .first()
+  .then(addressDetails => {
+    if (addressDetails) {
+      return addressDetails;
+    } else {
+     return knex('addresses')
+            .insert(address)
+            .returning('id')
+            .first()
+    }
+  })
+  .then(addressDetails  => {
     let contact = {
-      address_id : id[0],
+      address_id : addressDetails.id,
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       phone_number: req.body.phone_number,
       email_address: req.body.email_address,
       image_url: req.body.image_url
     }
-    knex('contacts')
+    return knex('contacts')
     .insert(contact)
     .returning('id')
-    .then(id => {
-      res.redirect(`/contacts/${id}`)
-    })
+  })
+  .then(id => {
+    res.redirect(`/contacts/${id}`)
   })
 });
 
 router.delete('/:id',(req,res,next) => {
     let id = req.params.id
     knex('contacts')
-    .del()
     .where({id})
+    .del()
     .returning('address_id')
     .then(addressID => {
       knex('contacts')
